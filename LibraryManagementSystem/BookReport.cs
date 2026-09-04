782b0748cdaa33dfc9520f3d54c81d29599270a6
Sithumini Anuhansi <anuhansisithumini@gmail.com>
2026-09-04 16:31:14 +0530

    feat: add book report screen and Crystal Report

---
+++ LibraryManagementSystem/BookReport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystem
+{
+    public partial class BookReport : Form
+    {
+        //connection to database
+        string cs = @"Data Source = Sithumini\SQLEXPRESS; Initial Catalog=LibraryManagementSystem; Integrated Security=True";
+
+        //default constructor
+        public BookReport()
+        {
+            InitializeComponent();
+        }
+
+        //loading form
+        private void BookReport_Load(object sender, EventArgs e)
+        {
+            this.crystalReportViewer1.ReportSource = @"C:\Users\sithu\OneDrive\Desktop\GUI\LibraryManagementSystem\LibraryManagementSystem\BookCrystalReport.rpt";
+        }
+    }
+}
